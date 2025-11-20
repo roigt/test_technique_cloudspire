4f72adcf64b2d763b6b05085b255c7cc60f74f7a
Godwin Julio ROIGT <griezmanjulio@yahoo.com>
2025-11-20 17:00:56 +0100

    initialisation

---
+++ app/Models/HotelPhoto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HotelPhoto extends Model
+{
+    /** @use HasFactory<\Database\Factories\HotelPhotoFactory> */
+    use HasFactory;
+}
